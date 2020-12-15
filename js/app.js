73845f9442c109b08b5e296d118368b1936ceb87
Mekrok <73012254+Mekrok@users.noreply.github.com>
2020-12-15 14:17:13 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
 let nav = $("#nav");
 
 $("#navToggle").on("click", function(event) {
-    event.preventDefault();
-    nav.toggleClass("show");
+    event.preventDefault(); /* действие по умолчанию не выполнится */
+    nav.toggleClass("show"); /* добавить класс */
 })
